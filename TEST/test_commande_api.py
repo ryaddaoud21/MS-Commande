a67cff5165e5c9f2af83636b64ab23a1fcee0280
Mohamed Ryad <65093859+ryaddaoud21@users.noreply.github.com>
2024-08-31 13:42:07 +0200

    Update test_commande_api.py

--- TEST/test_commande_api.py
+++ TEST/test_commande_api.py
@@ -85,10 +85,8 @@ class CommandeTestCase(unittest.TestCase):
         response = self.app.delete(f'/orders/{order.id}', headers={'Authorization': f'Bearer {self.admin_token}'})
         self.assertEqual(response.status_code, 200)
         response = self.app.get(f'/orders/{order.id}', headers={'Authorization': f'Bearer {self.admin_token}'})
-        #self.assertEqual(response.status_code, 404)
-        #self.assertEqual(response.status_code, 404)
-
-        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.status_code, 404)
+        #self.assertEqual(response.status_code, 200)
 
 
 if __name__ == '__main__':
