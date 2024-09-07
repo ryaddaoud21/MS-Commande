7e5fe7fd316f5e3221ed52de8fc40da6b3c9ab42
ryaddaoud21 <Chtaybo@22>
2024-09-08 01:31:53 +0200

    switcher la congif de la BDD en image docker mysql-db

--- API/config.py
+++ API/config.py
@@ -1,5 +1,6 @@
 import os
 
 class Config:
-    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URI', 'mysql+pymysql://root@localhost/commande_db')
+    #SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URI', 'mysql+pymysql://root@localhost/commande_db')
+    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URI', 'mysql+pymysql://root@mysql-db/commande_db')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
