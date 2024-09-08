205b65eb08c36b63af0a6cbb14df738c3f98c1f7
ryaddaoud21 <Chtaybo@22>
2024-09-08 11:43:32 +0200

    modifier get_rabbitmq_connection

--- API/services/pika_config.py
+++ API/services/pika_config.py
@@ -1,7 +1,12 @@
 import pika
+import time
 
 def get_rabbitmq_connection():
-    connection = pika.BlockingConnection(
-        pika.ConnectionParameters(host='rabbitmq')
-    )
-    return connection
+    while True:
+        try:
+            connection = pika.BlockingConnection(
+                pika.ConnectionParameters(host='rabbitmq'))
+            return connection
+        except pika.exceptions.AMQPConnectionError:
+            print("RabbitMQ n'est pas encore disponible. Nouvelle tentative dans 5 secondes...")
+            time.sleep(5)
