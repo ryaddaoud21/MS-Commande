4768ce19ca24b97930a2152fee2b3f3fbd8a80a6
ryaddaoud21 <Chtaybo@22>
2024-09-08 03:12:33 +0200

    ajout de route index /

--- API/auth.py
+++ API/auth.py
@@ -76,4 +76,4 @@ def logout():
 
 @auth_blueprint.route('/', methods=['GET'])
 def index():
-    return jsonify({"msg": "Welcome to the ORDERS'API. The service is up and running!"}), 200
+    return jsonify({"msg": "Welcome to the ORDER's API. The service is up and running!"}), 200
